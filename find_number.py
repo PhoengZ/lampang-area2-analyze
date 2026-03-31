08f760f06a68331721cb84857f815e32a80deb62
Phaolap Kulteera <pholapcondo11@gmail.com>
2026-03-31 22:13:50 +0700

    Update Code recheck all unit

--- find_number.py
+++ find_number.py
@@ -7,14 +7,21 @@ with open('election-stations-2569.json', 'r', encoding='utf-8') as f:
 # หาหน่วยเลือกตั้งของจังหวัด
 rayong = next((p for p in data['provinces'] if p['name'] == 'ระยอง'), None)
 # print(f"ระยอง {rayong['total_stations']} หน่วยเลือกตั้ง")
-
+d = dict()
 # หาหน่วยเลือกตั้งจากรหัส
 def find_total_station(area,province):
     for p in data['provinces']:
         if p['name'] == province:
             for a in p['areas']:
                 if a['area'] == area:
-                    return a['total_stations']
+                    for unit in a['stations']:
+                        if unit['district'] not in d:
+                            d[unit['district']] = dict()
+                        if unit['subdistrict'] not in  d[unit['district']]:
+                            d[unit['district']][unit['subdistrict']] = 1
+                        else:
+                            d[unit['district']][unit['subdistrict']] += 1
     return None
 
-print(find_total_station(2, 'ลำปาง'))
+find_total_station(2, 'ลำปาง')
+print(d)
